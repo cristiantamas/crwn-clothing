3335c0507408b67b9c9699656cf70abb025d826a
Cristian Tamas <cristiantamas05@gmail.com>
2022-05-21 12:57:05 +0300

    Added firebase auth

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,15 +1,18 @@
 import './category-item.styles.scss';
 
 const CategoryItem = ({ category }) => {
-  const { title, imageUrl } = category;
+  const { imageUrl, title } = category;
   return (
-    <div className="category-container">
-      <div className="background-image" style={{
-        backgroundImage: `url(${imageUrl})`
-      }} />
-      <div className="category-body-container">
+    <div className='category-container'>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='category-body-container'>
         <h2>{title}</h2>
-        <p>Shop now</p>
+        <p>Shop Now</p>
       </div>
     </div>
   );
