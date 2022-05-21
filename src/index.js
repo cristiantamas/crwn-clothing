af3c0eae9c2272d14b55000098c4b059d397db46
Cristian Tamas <cristiantamas05@gmail.com>
2022-05-21 15:43:11 +0300

    Added context for products

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
 
 import './index.scss';
 
@@ -13,9 +14,11 @@ const rootElement = document.getElementById('root');
 render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+        <UserProvider>
+          <ProductsProvider>
+              <App />
+          </ProductsProvider>
+        </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   rootElement
