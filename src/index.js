d1f92add80ebd9b1c182e23166db5641ecf8be95
Cristian Tamas <cristiantamas05@gmail.com>
2022-05-31 17:27:39 +0300

    Added redux-persist

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './store/store';
 
 import App from './App';
-
 import './index.scss';
 
 const rootElement = document.getElementById('root');
@@ -13,9 +14,11 @@ const rootElement = document.getElementById('root');
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
