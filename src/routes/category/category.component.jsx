54f8c1fcd27dde83fdcc6f66438a0e33222b925e
Cristian Tamas <cristiantamas05@gmail.com>
2022-06-02 17:17:29 +0300

    Added redux-thunk and implemented on categories

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,28 +1,37 @@
-import { useContext, useState, useEffect} from 'react';
+import { useState, useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
+
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 
 // import { CategoriesContext } from '../../contexts/categories.context';
 import { CategoryContainer, CategoryTitle } from './category.styles.jsx';
+
 
 const Category = () => {
   const { category } = useParams();
   // const { categoriesMap } = useContext(CategoriesContext);
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts ] = useState(categoriesMap[category]);
 
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
-  
+  console.log('+++Cristi', isLoading);
   return (
     <>
-        <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-      <CategoryContainer>
-        {products && products.map((product) => <ProductCard key={product.id} product={product} />)}
-      </CategoryContainer>
+      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+      {
+        isLoading 
+        ? <Spinner />
+        : (
+          <CategoryContainer>
+            {products && products.map((product) => <ProductCard key={product.id} product={product} />)}
+          </CategoryContainer>
+        )}
     </>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -3,20 +3,21 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user']
+  whitelist: ['user']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer, 
-  middleware: [process.env.NODE_ENV !== 'production' && logger].filter(Boolean),
+  middleware: [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean),
 });
 
 export const persistor = persistStore(store);
